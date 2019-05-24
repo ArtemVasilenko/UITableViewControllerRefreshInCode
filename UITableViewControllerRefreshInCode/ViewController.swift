33e60c3bcf7bc1a1e284854ca40d39ed883b32f1
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-05-24 21:18:31 +0300

    start

--- UITableViewControllerRefreshInCode/ViewController.swift
+++ UITableViewControllerRefreshInCode/ViewController.swift
@@ -1,20 +1,57 @@
-//
-//  ViewController.swift
-//  UITableViewControllerRefreshInCode
-//
-//  Created by Артем on 5/24/19.
-//  Copyright © 2019 Артем. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let tableVC = UITableViewController(style: .plain)
+    let id = "Cell"
+    var allTime = [Date]()
+    let refresh = UIRefreshControl()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        createTableView()
+        refresh.addTarget(self, action: #selector(handleRefresh), for: .valueChanged)
+        refresh.tintColor = .red
+        tableVC.tableView.addSubview(refresh)
+        self.view.addSubview(tableVC.tableView)
+    }
+    
+    func createTableView() {
+        tableVC.tableView.register(UITableViewCell.self, forCellReuseIdentifier: id)
+        tableVC.tableView.delegate = self
+        tableVC.tableView.dataSource = self
+    }
+    
+    @objc func handleRefresh() {
+        allTime.append(Date())
+        refresh.endRefreshing()
+        
+        let indexPathNewRow = IndexPath(row: allTime.count - 1, section: 0)
+        tableVC.tableView.insertRows(at: [indexPathNewRow], with: .left)
+        
+        
     }
 
 
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSourcePrefetching, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
+        print(indexPaths)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return allTime.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: id, for: indexPath)
+        let item = allTime[indexPath.row]
+        cell.textLabel?.text = "\(item)"
+        
+        return cell
+    }
+    
 }
 
